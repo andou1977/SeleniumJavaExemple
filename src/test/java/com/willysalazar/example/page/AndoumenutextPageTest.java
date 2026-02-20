9f00b3100e678cb41268fe0109ad97dc17c35e96
andou1977 <andouemmanuel@gmail.com>
2026-02-20 13:07:33 +0100

    first

--- src/test/java/com/willysalazar/example/page/AndoumenutextPageTest.java
+++ src/test/java/com/willysalazar/example/page/AndoumenutextPageTest.java
@@ -1,9 +1,8 @@
 package com.willysalazar.example.page;
 
 import com.willysalazar.example.utils.BaseTest;
-import org.junit.jupiter.api.Assertions;
+
 import org.testng.annotations.Test;
-import org.testng.asserts.Assertion;
 
 public class AndoumenutextPageTest extends BaseTest {
 
